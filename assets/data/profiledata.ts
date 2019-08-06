048698532adbf024c94c24eb617dbe32cebdf951
tinaongster <52726695+tinaongster@users.noreply.github.com>
2019-08-06 17:06:47 -0400

    Add files via upload

---
+++ assets/data/profiledata.ts
@@ -0,0 +1,9 @@
+import { tinaongster } from '../../app/tinaongster';
+
+export const MYPROFILE: tinaongster = {
+    snumber: 991508646, 
+    sname: 'Tina Ong', 
+    slogin: 'ongtin', 
+    campus: 'Davis', 
+    assignment: 'Mobile Dev A4'
+}
